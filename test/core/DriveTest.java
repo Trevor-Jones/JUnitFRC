d30d2d22f59c6c303b34ed1c4b7ce2218e14ccd3
Trevor-Jones <trevor.eric.j@gmail.com>
2016-08-23 15:08:03 -0700

    no more catch, added failure messages

--- test/core/DriveTest.java
+++ test/core/DriveTest.java
@@ -7,19 +7,19 @@ import core.Drive;
 public class DriveTest {
 
 	@Test
-	public void test() throws Exception {
- 		double[] answersLeft = {1,-.95,-1,0.6,1,0,-1,-.6,1,0.9,-1,-1};
+	public void test() {
+ 		double[] answersLeft = {1,-.95,-1,0.6,1,80,-1,-.6,1,0.9,-1,-1};
 		double[] answersRight = {-1,-1,0.81,1,-0.32,-1,-0.3,1,0.8,-1,-1,1};
 		Drive drive = new Drive();
 		for(int i = 0; i < 360; i+=30) {
 			double[] leftRight = drive.drive(1, i);
-			assertEquals(answersLeft[i/30],leftRight[0],0.02);
-			assertEquals(answersRight[i/30],leftRight[1],0.05);
+			assertEquals(String.format("Wow, test number %d of the left side was incorrect", i), Left[i/30],leftRight[0],0.05);
+			assertEquals(String.format("Wow, test number %d of the right side was incorrect", i), answersRight[i/30],leftRight[1],0.05);
 		}
 	}
 	
 	@Test
-	public void equalsTest() throws Exception {
-		assertEquals(0,1);
+	public void equalsTest() {
+		assertEquals(0,0);
 	}
 }
