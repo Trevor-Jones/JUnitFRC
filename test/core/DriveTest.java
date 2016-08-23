3b1e36b10eb01115ac13e890e10f8366cda0662b
Trevor-Jones <trevor.eric.j@gmail.com>
2016-08-23 15:28:31 -0700

    tests should be working now

--- test/core/DriveTest.java
+++ test/core/DriveTest.java
@@ -8,7 +8,7 @@ public class DriveTest {
 
 	@Test
 	public void test() {
- 		double[] answersLeft = {1,-.95,-1,0.6,1,80,-1,-.6,1,0.9,-1,-1};
+ 		double[] answersLeft = {1,-.95,-1,0.6,1,0,-1,-.6,1,0.9,-1,-1};
 		double[] answersRight = {-1,-1,0.81,1,-0.32,-1,-0.3,1,0.8,-1,-1,1};
 		Drive drive = new Drive();
 		for(int i = 0; i < 360; i+=30) {
